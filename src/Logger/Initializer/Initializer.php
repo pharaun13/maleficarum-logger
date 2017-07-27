f47867eede97bb40dbe3dccd67c16a3583196aac
damian.sromek <damian.sromek@miinto.com>
2017-07-27 17:46:55 +0200

    [Dev] Fix indentation in Initializer

--- src/Logger/Initializer/Initializer.php
+++ src/Logger/Initializer/Initializer.php
@@ -12,38 +12,38 @@ namespace Maleficarum\Logger\Initializer;
 
 class Initializer {
 
-	/* ------------------------------------ Class Methods START ---------------------------------------- */
+    /* ------------------------------------ Class Methods START ---------------------------------------- */
 
-	/**
-	 * This method will initialize the entire package.
+    /**
+     * This method will initialize the entire package.
      *
      * @param array $opts Container options
-	 * @return string
-	 */
-	static public function initialize(array $opts = []) : string {
-		// load default builder if skip not requested
-		$builders = $opts['builders'] ?? [];
-		is_array($builders) or $builders = [];
-		if (!isset($builders['logger']['skip'])) {
-			\Maleficarum\Ioc\Container::register('Monolog\Logger', function () use ($opts) {
-				$logger = new \Monolog\Logger('api');
-				$prefix = isset($opts['prefix']) ? $opts['prefix'] : "Maleficarum";
-				$handler = new \Monolog\Handler\SyslogHandler('[PHP]['.$prefix.'][Api]', \LOG_USER, \Monolog\Logger::DEBUG, true, \LOG_PID);
-				$logger->pushHandler($handler);
-				if (isset($opts['logger.message_prefix'])) {
-				    self::addMessagePrefixProcessor($logger, $opts['logger.message_prefix']);
+     * @return string
+     */
+    static public function initialize(array $opts = []) : string {
+        // load default builder if skip not requested
+        $builders = $opts['builders'] ?? [];
+        is_array($builders) or $builders = [];
+        if (!isset($builders['logger']['skip'])) {
+            \Maleficarum\Ioc\Container::register('Monolog\Logger', function () use ($opts) {
+                $logger = new \Monolog\Logger('api');
+                $prefix = isset($opts['prefix']) ? $opts['prefix'] : "Maleficarum";
+                $handler = new \Monolog\Handler\SyslogHandler('[PHP]['.$prefix.'][Api]', \LOG_USER, \Monolog\Logger::DEBUG, true, \LOG_PID);
+                $logger->pushHandler($handler);
+                if (isset($opts['logger.message_prefix'])) {
+                    self::addMessagePrefixProcessor($logger, $opts['logger.message_prefix']);
                 }
 
-				return $logger;
-			});
-		} 
+                return $logger;
+            });
+        }
 
-		// load logger object
-		\Maleficarum\Ioc\Container::registerDependency('Maleficarum\Logger', \Maleficarum\Ioc\Container::get('Monolog\Logger'));
+        // load logger object
+        \Maleficarum\Ioc\Container::registerDependency('Maleficarum\Logger', \Maleficarum\Ioc\Container::get('Monolog\Logger'));
 
-		// return initializer name
-		return __METHOD__;
-	}
+        // return initializer name
+        return __METHOD__;
+    }
 
     /**
      * @param \Monolog\Logger $logger
@@ -57,6 +57,6 @@ class Initializer {
         });
     }
 
-	/* ------------------------------------ Class Methods END ------------------------------------------ */
+    /* ------------------------------------ Class Methods END ------------------------------------------ */
 
 }
