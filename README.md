520dbee30e3d02ed5eee40e281c58e2f330e6837
damian.sromek <damian.sromek@miinto.com>
2017-07-27 17:35:58 +0200

    Make it possible to set message prefix

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Change Log
 This is the Maleficarum Logger component. It uses monolog as it bases but provides initializers and builders for use in api/proxy/worker projects.
 
+## [1.1.0] - 2017-07-27
+### Added
+- Make it possible to set log message prefix
+
 ## [1.0.0] - 2017-03-24
 ### Added
 - This is an initial release of the component - based on the code written by me and included inside the maleficarum API repository.
--- src/Logger/Initializer/Initializer.php
+++ src/Logger/Initializer/Initializer.php
@@ -1,6 +1,10 @@
 <?php
 /**
  * This class carries ioc initialization functionality used by this component.
+ *
+ * Container options:
+ * - string 'prefix' (optional) 'Maleficarum' by default; Will use given prefix in log identifier.
+ * - string 'logger.message_prefix' (optional) if provided will result in given prefix being added to every log message
  */
 declare (strict_types=1);
 
@@ -12,6 +16,8 @@ class Initializer {
 
 	/**
 	 * This method will initialize the entire package.
+     *
+     * @param array $opts Container options
 	 * @return string
 	 */
 	static public function initialize(array $opts = []) : string {
@@ -19,11 +25,15 @@ class Initializer {
 		$builders = $opts['builders'] ?? [];
 		is_array($builders) or $builders = [];
 		if (!isset($builders['logger']['skip'])) {
-			\Maleficarum\Ioc\Container::register('Monolog\Logger', function () {
+			\Maleficarum\Ioc\Container::register('Monolog\Logger', function () use ($opts) {
 				$logger = new \Monolog\Logger('api');
 				$prefix = isset($opts['prefix']) ? $opts['prefix'] : "Maleficarum";
 				$handler = new \Monolog\Handler\SyslogHandler('[PHP]['.$prefix.'][Api]', \LOG_USER, \Monolog\Logger::DEBUG, true, \LOG_PID);
 				$logger->pushHandler($handler);
+				if (isset($opts['logger.message_prefix'])) {
+				    self::addMessagePrefixProcessor($logger, $opts['logger.message_prefix']);
+                }
+
 				return $logger;
 			});
 		} 
@@ -34,6 +44,18 @@ class Initializer {
 		// return initializer name
 		return __METHOD__;
 	}
+
+    /**
+     * @param \Monolog\Logger $logger
+     * @param string $messagePrefix
+     */
+	private static function addMessagePrefixProcessor(\Monolog\Logger $logger, string $messagePrefix) {
+        $logger->pushProcessor(function (array $record) use ($messagePrefix) {
+            $record['message'] = $messagePrefix . $record['message'];
+
+            return $record;
+        });
+    }
 
 	/* ------------------------------------ Class Methods END ------------------------------------------ */
 
